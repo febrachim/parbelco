3b1af1e64eaac32c5e69e949a88be35acdafbc3f
febryan rachim <febryan.rachim@gmail.com>
2016-11-07 18:55:52 +0700

    obrak abrik landing page. wkwkwk

--- js/bin/init.js
+++ js/bin/init.js
@@ -18,8 +18,45 @@
             loading: true,
             loadingParentElement: 'body', //animsition wrapper element
             loadingClass: 'animsition-loading',
-            loadingInner: '<img src="squares.svg" />', // e.g '<img src="loading.svg" />'
+            loadingInner: '' // e.g '<img src="loading.svg" />'
         });
+
+//        smooth scroll target
+        $('a').click(function() {
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top
+            }, 500);
+            return false;
+        });
+
+//        scrollFire
+        var options = [
+            {selector: '#speck-of-dust', offset: 200, callback: function(el) {
+                    $(el).typed({
+                        strings: ["just a speck of dust within the galaxy..."],
+                        loop: false,
+                        // typing speed
+                        typeSpeed: 0,
+                        // backspacing speed
+                        backSpeed: 10,
+                        // time before backspacing
+                        backDelay: 1700,
+                        // show cursor
+                        showCursor: true,
+                        //callback for every typed string
+                        onStringTyped: function() {
+                            $('#speck-btn').fadeIn("slow");
+                        }
+                    });
+                }},
+            {selector: '#staggered-test', offset: 400, callback: function(el) {
+                    Materialize.showStaggeredList($(el));
+                }},
+            {selector: '#image-test', offset: 500, callback: function(el) {
+                    Materialize.fadeInImage($(el));
+                }}
+        ];
+        Materialize.scrollFire(options);
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
